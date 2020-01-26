5fe780c86d6ad44939cd0a8012bc43ff1c14a47b
brunurb <brunurb+github@gmail.com>
2020-01-26 18:12:41 +0000

    Add files via upload

---
+++ SCRIPTS/Scrot/auto-screenshot.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+scrot -u -q 1 '%Y-%m-%d-%H:%M:%S.png' -e 'mv $f /home/bbb/Videos/MAPStimelapse/'
---
+++ SCRIPTS/Scrot/auto-screenshotz.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+do
+scrot -d 1 '%Y-%m-%d-%H:%M:%S.png' -e 'mv $f /home/bbb/Videos/MAPStimelapse';
+done
