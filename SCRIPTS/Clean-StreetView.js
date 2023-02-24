1c69c06a974fc4fb7fa188f05cbc25c432cfa27b
brunurb <brunurb+github@gmail.com>
2023-02-24 15:13:33 +0000

    Create Clean-StreetView.js

---
+++ SCRIPTS/Clean-StreetView.js
@@ -0,0 +1,6 @@
+<p>Drag the link into your bookmarks.</p>
+<h2> >>> <a href="javascript:var%20ids%20=%20[&quot;titlecard&quot;,%20&quot;image-header&quot;,%20&quot;fineprint&quot;,%20&quot;&quot;];var%20hidden%20=%20(window.getComputedStyle(document.getElementById(ids[0]))).getPropertyValue(&quot;display&quot;);if%20(hidden%20!==%20&quot;none&quot;)%20{%20var%20disp%20=%20&quot;none&quot;;%20document.getElementsByClassName('app-viewcard-strip')[0].style.visibility='hidden';}%20else%20{%20var%20disp%20=%20&quot;&quot;;%20document.getElementsByClassName('app-viewcard-strip')[0].style.visibility='';}for%20(var%20i%20=%200;%20i%20<%20ids.length;%20i++)%20{%20%20document.getElementById(ids[i]).style.display%20=%20disp;}">Clean Street View</a> <<< </h2>
+
+<p>When in Street View, click on the bookmark to remove the clutter. Click again to undo if needed.</p>
+<p>
+<sub>This is an updated version of the original code from <a href="http://dddavemaps.blogspot.com/2015/04/streetview-cleaner-bookmarklet.html">dddavemaps.blogspot.com</a></sub></p>
